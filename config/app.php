95caa8d668d589c7ecdc9357d3aede422014da3d
Fernando Bevilacqua <dovyski@gmail.com>
2021-01-13 16:04:40 -0300

    fix(layout) problema na visualização quando servido como proxy reserso

--- config/app.php
+++ config/app.php
@@ -56,6 +56,8 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
+    'mix_url' => env('APP_URL', 'http://localhost'),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
