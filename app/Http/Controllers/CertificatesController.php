723ab64ec5b9b1e1f59aa2d252ff6f2e204bf83d
Matheus Vieira Santos <matheus.eu.mv@gmail.com>
2022-04-26 20:01:48 -0300

    Finalizado front para demonstrar certificados capturados

--- app/Http/Controllers/CertificatesController.php
+++ app/Http/Controllers/CertificatesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\DB;
 
 class CertificatesController extends Controller
 {
@@ -15,10 +16,12 @@ class CertificatesController extends Controller
     public function show(Request $request)
     {
         $user = $request->user();
+        $data = DB::select("select * from certificates where (user_id={$user->id})");
 
         return view('certificates.show', [
             'request' => $request,
             'user' => $request->user(),
+            'data' => $data,
         ]);
     }
 }
